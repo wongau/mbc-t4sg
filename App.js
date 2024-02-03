43f8018449f00042f362a0ae968351b49614505a
Edison Tang <84212428+Edison-T@users.noreply.github.com>
2024-02-03 15:24:23 -0500

    fixed image source and image styling
    the border around the image is there just for testing

--- App.js
+++ App.js
@@ -3,30 +3,79 @@ import { Button, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-n
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={ {color: "black", fontFamily: "Avenir Next", fontWeight: "600", fontSize: 20, marginBottom: 25}}>Maize and Blue Cupboard</Text>
-      <Image source={require("./assets/MBC-signature-vertical-blue.png")} style={{width: "100%", height: 250, marginBottom: 50}}/>
-    
-      <TouchableOpacity style={{marginBottom: 25}} activeOpacity={0.7}>
-        <View style={{ backgroundColor: "gray", width: 300, height: 50, borderRadius: 20, alignItems: "center", justifyContent: "center"}}>
-          <Text style={ {color: "white", fontFamily: "Avenir Next", fontWeight: "600", fontSize: 20}}> login </Text>
-        </View>
+    <View style={styles.mainContainer}>
+      <Text style={styles.welcomeText}>
+        Welcome to
+      </Text>
+
+      <View style={styles.imageParentView}>
+        <Image 
+          source={require("./assets/MBC-signature-vertical-blue.png")} 
+          style={styles.image}/>
+      </View>  
+
+      <TouchableOpacity style={styles.loginButton} activeOpacity={0.7}>
+          <Text style={styles.buttonText}>
+            Login
+          </Text>
       </TouchableOpacity>
 
-      <TouchableOpacity activeOpacity={0.7}>
-        <View style={{ backgroundColor: "gray", width: 300, height: 50, borderRadius: 20, alignItems: "center", justifyContent: "center"}}>
-          <Text style={ {color: "white", fontFamily: "Avenir Next", fontWeight: "600", fontSize: 20}}> signup </Text>
-        </View>
+      <TouchableOpacity style={styles.signUpButton} activeOpacity={0.7}>
+          <Text style={styles.buttonText}>
+            Sign Up
+          </Text>
       </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  welcomeText: {
+    color: "black", 
+    fontFamily: "Avenir Next", 
+    fontWeight: "600", 
+    fontSize: 20, 
+    marginBottom: 25
+  },
+  imageParentView: {
+    width: "70%", 
+    height: "20%", 
+    borderColor:'black', 
+    borderWidth: 3, 
+    marginBottom: 50
+  },
+  image: {
+    flex: 1, 
+    width: null, 
+    height: null, 
+    resizeMode: "contain"
+  },
+  loginButton: {
+    marginBottom: 25, 
+    backgroundColor: "gray", 
+    width: 300, height: 50, 
+    borderRadius: 20, 
+    alignItems: "center", 
+    justifyContent: "center"
+  },
+  buttonText: {
+    color: "white", 
+    fontFamily: "Avenir Next", 
+    fontWeight: "600", 
+    fontSize: 20
+  },
+  signUpButton: {
+    backgroundColor: "gray", 
+    width: 300, 
+    height: 50, 
+    borderRadius: 20, 
+    alignItems: "center", 
+    justifyContent: "center"
+  }
 });
