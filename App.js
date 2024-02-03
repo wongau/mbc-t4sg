ec5b9dc027903631ad97a700ad31c88ec29f20aa
Edison Tang <84212428+Edison-T@users.noreply.github.com>
2024-02-03 15:00:34 -0500

    Update image source because you already had an assets folder

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={ {color: "black", fontFamily: "Avenir Next", fontWeight: "600", fontSize: 20, marginBottom: 25}}>Maize and Blue Cupboard</Text>
-      <Image source={require("./Assets/MBC-signature-vertical-blue.png")} style={{width: "100%", height: 250, marginBottom: 50}}/>
+      <Image source={require("./assets/MBC-signature-vertical-blue.png")} style={{width: "100%", height: 250, marginBottom: 50}}/>
     
       <TouchableOpacity style={{marginBottom: 25}} activeOpacity={0.7}>
         <View style={{ backgroundColor: "gray", width: 300, height: 50, borderRadius: 20, alignItems: "center", justifyContent: "center"}}>
