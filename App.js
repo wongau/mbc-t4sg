fd5780fbb0108949151786ec1449eb574368d923
wongau <wongau@umich.edu>
2024-02-03 14:17:57 -0500

    rough home screen

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! </Text>
-      <StatusBar style="auto" />
+      <Text style={ {color: "black", fontFamily: "Avenir Next", fontWeight: "600", fontSize: 20, marginBottom: 25}}>Maize and Blue Cupboard</Text>
+      <Image source={require("./Assets/MBC-signature-vertical-blue.png")} style={{width: "100%", height: 250, marginBottom: 50}}/>
+    
+      <TouchableOpacity style={{marginBottom: 25}} activeOpacity={0.7}>
+        <View style={{ backgroundColor: "gray", width: 300, height: 50, borderRadius: 20, alignItems: "center", justifyContent: "center"}}>
+          <Text style={ {color: "white", fontFamily: "Avenir Next", fontWeight: "600", fontSize: 20}}> login </Text>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity activeOpacity={0.7}>
+        <View style={{ backgroundColor: "gray", width: 300, height: 50, borderRadius: 20, alignItems: "center", justifyContent: "center"}}>
+          <Text style={ {color: "white", fontFamily: "Avenir Next", fontWeight: "600", fontSize: 20}}> signup </Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 }
